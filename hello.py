5b6ac2dd9ff3524f64801624ca3da22f6fd72087
wangzz2019 <jack.wang@datadoghq.com>
2021-09-03 20:18:48 +0900

    typo

--- hello.py
+++ hello.py
@@ -16,5 +16,5 @@ def page2():
     return "<p>Hi, This is page 12</p>"
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP','127.0.0.1'),port=int(os.getenv('PORT',5000)),debut=True)
+    app.run(host=os.getenv('IP','127.0.0.1'),port=int(os.getenv('PORT',5000)),debug=True)
 
