c5ba7d5b3afd88e622f4ae50c7384502a1649c5c
wangzz2019 <jack.wang@datadoghq.com>
2022-11-16 11:19:26 +0900

    change default port to 5002

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from markupsafe import escape
 import os
+import time
+from ddtrace import tracer
 
 app = Flask(__name__)
 
@@ -16,10 +18,34 @@ def page1():
 def routename(name):
     return f"Hello, {escape(name)}!"
 
+@app.route("/methodpage")
+def methodpage():
+    with tracer.trace("testtrace") as outer_span:
+        time.sleep(1)
+        with tracer.trace("method1") as span:
+            method1()
+        time.sleep(1)
+        with tracer.trace("method2") as span:
+            method2()
+    return "<p>Method1 and Method2 run. Please check the console output</p>"
+
+
 @app.route("/page2")
 def page2():
     return "<p>Hi, This is page 16</p><br/><a href='/'>back</a>"
 
+def method1():
+    print("Method1 is running....")
+    time.sleep(2)
+    print("Method1 finished")
+
+
+def method2():
+    print("Method2 is running....")
+    time.sleep(3)
+    print("Method2 finished")
+
+
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP','0.0.0.0'),port=os.getenv('PORT',5000),debug=True)
+    app.run(host=os.getenv('IP','0.0.0.0'),port=os.getenv('PORT',5002),debug=True)
 
